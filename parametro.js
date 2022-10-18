a45680a8db6a15113a8b5975b0538918ded674aa
lucaspaulamonti <lucasdepaulamonti@gmail.com>
2022-10-18 00:11:48 -0300

    Create parametro.js

---
+++ parametro.js
@@ -0,0 +1,13 @@
+// Validando um parâmetro passado para a função.
+function getMeanTemp(temperatures) {
+    if (!(temperatures instanceof Array)) {
+        return NaN;
+    }
+    let sum = 0;
+    for (let i = 0; i < temperatures.length; i++) {
+        sum += temperatures[i];
+    }
+    return sum / temperatures.length;
+}
+console.log(getMeanTemp(10));
+console.log(getMeanTemp([10, 30]));
